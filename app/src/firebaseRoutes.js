c70fafe557ff9860ae24d3d6570422a4570f97be
jeremysu99 <jeremysu99@gmail.com>
2024-11-18 23:47:42 -0800

    updates routes

--- app/src/firebaseRoutes.js
+++ app/src/firebaseRoutes.js
@@ -2,6 +2,23 @@ import { getDatabase, ref, get, onValue } from "firebase/database";
 
 import { database } from "./firebase";
 
+// Used to read data once (best for users and curr user index)
+export const fetchDataOnce = async (path) => {
+    const dbRef = ref(database, path); // Create a reference to the path in Firebase Realtime Database
+    try {
+      const snapshot = await get(dbRef); // Fetch the data once
+      if (snapshot.exists()) {
+        return snapshot.val(); // Return the data
+      } else {
+        return null; // Return null if no data exists at the path
+      }
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+      return null; // Return null in case of an error
+    }
+  };
+
+// Used to update realtime data (best for trash level)
 export const listenToData = (path, callback) => {
     const dbRef = ref(database, path);
     onValue(dbRef, (snapshot) => {
